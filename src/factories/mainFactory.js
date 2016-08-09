13604fa7d907c9dd2a66eaa3feae1a32a7117cb3
Eric <ericso0303@gmail.com>
2016-08-08 21:30:55 -0700

    [Feature] Added http get request to main api

--- src/factories/mainFactory.js
+++ src/factories/mainFactory.js
@@ -1,7 +1,11 @@
 angular.module('factoryModule', [])
 
 //GET http://www.cheapshark.com/api/1.0/deals
-.factory('mainFactory', function(){
+.factory('mainFactory', function($http){
+	var rootUrl = "http://www.cheapshark.com/api/1.0/deals";
 
-	return {};
+	function getMovieData(){
+		return $http.get(rootUrl);
+	}
+	return { getMovieData: getMovieData };
 })
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 angular.module('Broadbean', ['factoryModule'])
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl', function($scope, mainFactory){
+
+	mainFactory.getMovieData().then(function(data){
+		console.log("movie data: ", data);
+	})
 	console.log("bootstrapped mainCtrl!");
-	
+
 })
