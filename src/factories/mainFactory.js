6757377ef3a3dee61cda632a2aee4fad6557ca6e
Eric <ericso0303@gmail.com>
2016-08-08 21:22:39 -0700

    [Architecture] basic front end laid out

---
+++ src/factories/mainFactory.js
@@ -0,0 +1,7 @@
+angular.module('factoryModule', [])
+
+//GET http://www.cheapshark.com/api/1.0/deals
+.factory('mainFactory', function(){
+
+	return {};
+})
---
+++ src/main.js
@@ -0,0 +1,5 @@
+angular.module('Broadbean', ['factoryModule'])
+.controller('mainCtrl', function($scope){
+	console.log("bootstrapped mainCtrl!");
+	
+})
