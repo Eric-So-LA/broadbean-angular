5bac22f3a026788ce4ef330e03e2995ad4f2b8f8
Eric <ericso0303@gmail.com>
2016-08-08 21:35:53 -0700

    [Feature] Display data on index.html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 angular.module('Broadbean', ['factoryModule'])
 .controller('mainCtrl', function($scope, mainFactory){
 
+	$scope.movieData = [];
 	mainFactory.getMovieData().then(function(data){
+		$scope.movieData = data.data;
 		console.log("movie data: ", data);
 	})
 	console.log("bootstrapped mainCtrl!");
